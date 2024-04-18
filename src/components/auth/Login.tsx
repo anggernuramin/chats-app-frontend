33a461880c7653d358cb7976d1fe808057e082f4
anggernuramin <anggern514@gmail.com>
2024-04-18 13:32:03 +0700

    feature: authentication with jsonwebtoken

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,40 +1,79 @@
 import {
-  Box,
   Button,
   FormControl,
   FormLabel,
   Input,
   InputGroup,
   InputRightElement,
+  Spinner,
   Text,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import LayoutForm from "./LayoutForm";
+import axios from "axios";
 
 const Login = () => {
-  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
   const [isShowPassword, setIsShowPassword] = useState(false);
-
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+  const navigate = useNavigate();
   const handleTooglePassword = () => {
     setIsShowPassword((state) => !state);
+  };
+  const handleLogin = async () => {
+    if (!email || !password) {
+      toast({
+        title: "Lengkapi filed nya!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+      return;
+    }
+
+    try {
+      setIsLoading(true);
+      const data = {
+        email,
+        password,
+      };
+      const response = await axios.post(
+        "http://localhost:3000/api/user/login",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      toast({
+        title: "Success Register!",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+      localStorage.setItem("userinfo", JSON.stringify(response?.data?.name));
+      setIsLoading(false);
+      setEmail("");
+      setPassword("");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
   };
   return (
     <LayoutForm>
       <VStack spacing={"5px"} w={"500px"}>
         <h2 className="header-form">Lengkapi Form Ini</h2>
-        <FormControl id="name" isRequired>
-          <FormLabel>Name</FormLabel>
-          <Input
-            type="text"
-            placeholder={"Enter your name"}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </FormControl>
+
         <FormControl id="email" isRequired>
           <FormLabel>Email</FormLabel>
           <Input
@@ -59,8 +98,15 @@ const Login = () => {
           </InputGroup>
         </FormControl>
 
-        <Button my={"4"} w={"100%"} bg={"#008069"} color={"white"}>
-          Login
+        <Button
+          onClick={handleLogin}
+          my={"4"}
+          w={"100%"}
+          bg={"#008069"}
+          color={"white"}
+          _hover={{ bg: "#00a884", transitionDuration: "0.3s" }}
+        >
+          {isLoading ? <Spinner /> : "Login"}
         </Button>
         <Text fontSize={".9rem"}>
           Belum Punya Akun ?{" "}
--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -6,13 +6,15 @@ import {
   Input,
   InputGroup,
   InputRightElement,
+  Spinner,
   Text,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
-import { color } from "framer-motion";
-import React, { useState } from "react";
+import { useState } from "react";
 import LayoutForm from "./LayoutForm";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Signup = () => {
   const [name, setName] = useState("");
@@ -21,15 +23,127 @@ const Signup = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [picture, setPicture] = useState("");
   const [isShowPassword, setIsShowPassword] = useState(false);
-
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+  const navigate = useNavigate();
   const handleTooglePassword = () => {
     setIsShowPassword((state) => !state);
   };
-  const postDetailImage = (e: any) => {};
+
+  const postDetails = (pics: File | null) => {
+    setLoading(true);
+
+    if (pics === undefined) {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+      return;
+    }
+
+    if (pics.type !== "image/jpeg" && pics.type !== "image/png") {
+      toast({
+        title: "Please Select a JPEG or PNG Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "chat-app");
+      data.append("cloud_name", "dlav3c2dh");
+      axios
+        .post("https://api.cloudinary.com/v1_1/dlav3c2dh/image/upload", data)
+        .then((response) => {
+          console.log("Cloudinary response:", response);
+          setPicture(response.data.url.toString());
+          setLoading(false);
+          toast({
+            title: "Image uploaded successfully!",
+            status: "success",
+            duration: 5000,
+            isClosable: true,
+            position: "bottom",
+          });
+        })
+        .catch((error) => {
+          console.log("Cloudinary error:", error);
+          setLoading(false);
+        });
+    }
+  };
+  const handleSignup = async () => {
+    if (!name || !email || !password || !picture) {
+      setLoading(false);
+
+      toast({
+        title: "Lengkapi filed nya!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+
+      return;
+    }
+    if (password !== confirmPassword) {
+      toast({
+        title: "Password tidak sama!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+      return;
+    }
+    try {
+      setLoading(true);
+      const data = {
+        name,
+        email,
+        password,
+        picture,
+      };
+
+      const response = await axios.post(
+        "http://localhost:3000/api/user/register",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      toast({
+        title: "Success Register!",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+      setLoading(false);
+
+      localStorage.setItem("userinfo", JSON.stringify(response?.data?.name));
+      navigate("/");
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
   return (
     <LayoutForm>
       <VStack spacing={"5px"}>
-        <h2 className="header-form">Lengkapi Form Ini</h2>
+        <h2 className="header-form">Silahkan Sign Up terlebih Dahulu.</h2>
         <FormControl id="name" isRequired>
           <FormLabel>Name</FormLabel>
           <Input
@@ -52,7 +166,7 @@ const Signup = () => {
             <InputGroup>
               <Input
                 type={isShowPassword ? "text" : "password"}
-                placeholder={"1234567"}
+                placeholder={"******"}
                 onChange={(e) => setPassword(e.target.value)}
               />
               <InputRightElement>
@@ -67,7 +181,7 @@ const Signup = () => {
             <InputGroup>
               <Input
                 type={isShowPassword ? "text" : "password"}
-                placeholder={"1234567"}
+                placeholder={"******"}
                 onChange={(e) => setConfirmPassword(e.target.value)}
               />
               <InputRightElement>
@@ -85,11 +199,18 @@ const Signup = () => {
             p={1.5}
             type="file"
             accept="image/*"
-            onChange={(e) => postDetailImage(e.target.files[0])}
+            onChange={(e) => postDetails(e.target.files[0])}
           />
         </FormControl>
-        <Button mt={"4"} w={"100%"} bg={"#008069"} color={"white"}>
-          Sign Up
+        <Button
+          onClick={handleSignup}
+          mt={"4"}
+          w={"100%"}
+          bg={"#008069"}
+          color={"white"}
+          _hover={{ bg: "#00a884", transitionDuration: "0.3s" }}
+        >
+          {loading ? <Spinner /> : "Sign Up"}
         </Button>
         <Text fontSize={".9rem"}>
           Sudah Punya Akun ?{" "}
